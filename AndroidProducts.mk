db2e28eb67d2929f3cb95a2fae1b140a5141c210
Andy | アンディ <54360298+nullptr03@users.noreply.github.com>
2023-08-08 20:18:21 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/voltage_RMX3461.mk
+    $(LOCAL_DIR)/lineage_RMX3461.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_RMX3461-user \
-    voltage_RMX3461-userdebug \
-    voltage_RMX3461-eng
+    lineage_RMX3461-user \
+    lineage_RMX3461-userdebug \
+    lineage_RMX3461-eng
